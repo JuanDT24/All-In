b7be7c0a12aa8bac92d2b2bb8c64311d6542c0a4
Alejorotp <98970394+Alejorotp@users.noreply.github.com>
2024-12-01 12:31:33 -0500

    Creación de user.py y conexión correcta a la base de datos

---
+++ backend/modules/user.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__ (self, Email, Password, Name, Lastname, PhoneNumber, Address, StarsSeller, StarsCustomer)
+        self.Email = Email
+        self.Password = Password
+        self.Name = Name
+        self.Lastname = Lastname
+        self.PhoneNumber = PhoneNumber
+        self.Address = Address
+        self.StarsSeller = StarsSeller
+        self.StarsCustomer = StarsCustomer
