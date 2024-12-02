05f98330749a48b9f2543734b0df58ed793b647e
JuanDT24 <tiradodj@uninorte.edu.co>
2024-12-01 21:11:30 -0500

    Updated item module

--- backend/modules/item.py
+++ backend/modules/item.py
@@ -1,6 +1,16 @@
 class Item:
-    def __init__(name, description, idSeller):
+    def __init__(name, description, idSeller, startingPrice, inmediate_Purchase_Price, minimum_Increase, post_Date, start_Date, due_Date, categories):
         self.name = name 
         self.description = description
         self.idSeller = idSeller
+        self.startingprice = startingPrice
+        self.inmediate_purchase_price = inmediate_Purchase_Price
+        self.minimum_Increase = minimum_Increase 
+        self.images = {}
+        self.post_Date = post_Date
+        self.start_Date = start_Date
+        self.due_Date = due_Date
+        self.categories = []
+        
+
         
