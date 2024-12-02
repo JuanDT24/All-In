581f7fd65396f410cda2464f6643039e7125d1de
JuanDT24 <tiradodj@uninorte.edu.co>
2024-12-02 02:08:16 -0500

    itemController

---
+++ All-In/backend/server.py
@@ -0,0 +1,14 @@
+  
+from flask import render_template, url_for, jsonify
+from database_controller import client
+import os
+from dotenv import load_dotenv
+app = Flask(__name__, template_folder = '../frontend', static_folder = '../frontend/css')
+@app.route('/')
+@app.route('/home')
+def home_page():
+    ##Render template handles requests to direct them to a html file
+    print(client.query("Select * from users"))
+    return render_template('index.html')
+if __name__=='__main__':
+    app.run(host='0.0.0.0', debug=True)
---
+++ backend/controllers/itemController.py
@@ -0,0 +1,36 @@
+from modules.item import item
+from database_controller import client
+
+class itemController():
+    _contador_id : int
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        # Si no existe la instancia, se crea una nueva
+        if cls._instance is None:
+            cls._instance = super(itemController, cls).__new__(cls)
+        return cls._instance
+    def createItem(self, name, description, idSeller, currentPrice, startingPrice, inmediate_Purchase_Price, minimum_Increase, post_Date, start_Date, due_Date, idcategory, image):
+        _contador_id = client.query("Select max(iditem) from items")[0]['max'] + 1
+        client.query(f"Insert into items(iditem, name, description, idseller, idcategory, image) VALUES ({self._contador_id}, '{name}', '{description}', {idSeller}, {idcategory}, {image})")
+        client.query(f"Insert into itempricesettings(iditem, price, startingprice, inmediatepurchaseprice, minimumincrease) VALUES ({self._contador_id}, {currentPrice}, {startingPrice}, {inmediate_Purchase_Price}, {minimum_Increase})")
+        client.query(f"Insert into itemdate(iditem, postingdate, startingdate, duedate) VALUE ({_contador_id}, {post_Date}, {start_Date}, {due_Date})")
+    def deleteItem(self, id):
+        client.query(f"Delete from items where id = {id} ")
+    def getItem(self, id):
+        result = (f"Select from items where id = {id}")
+        if result:
+            item_data = result[0]
+            item = Item(
+                item_data['iditem'],
+                item_data['name'],
+                item_data['description'],
+                item_data['idseller'],
+                item_data['idbuyer'],
+                item_data['idcategory'],
+                item_data['image'],
+             ) 
+             r
+        else: 
+            return f"Couldn't find item with id: {id}"
+    
