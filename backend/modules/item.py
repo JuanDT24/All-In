3c9f825ba9ad1151cffd1b40a813b8ed0aec04bd
JuanDT24 <tiradodj@uninorte.edu.co>
2024-12-02 00:39:47 -0500

    Commit to push item module

--- backend/modules/item.py
+++ backend/modules/item.py
@@ -1,16 +1,16 @@
 class Item:
-    def __init__(name, description, idSeller, startingPrice, inmediate_Purchase_Price, minimum_Increase, post_Date, start_Date, due_Date, categories):
+    def __init__(name, description, idSeller, currentPrice,startingPrice, inmediate_Purchase_Price, minimum_Increase, post_Date, start_Date, due_Date, idcategory, image):
         self.name = name 
         self.description = description
         self.idSeller = idSeller
         self.startingprice = startingPrice
         self.inmediate_purchase_price = inmediate_Purchase_Price
         self.minimum_Increase = minimum_Increase 
-        self.images = {}
+        self.images = image
         self.post_Date = post_Date
         self.start_Date = start_Date
         self.due_Date = due_Date
-        self.categories = []
+        self.categories = idcategory
         
 
         
